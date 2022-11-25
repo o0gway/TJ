5f0168bccb39b204cf2aa3a75a93c4bedb5f343f
Wenzel <kachman87@gmail.com>
2022-11-25 19:59:29 +0400

    Refactoring MyFirstGame.java

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,24 +1,21 @@
 class MyFirstGame {
     public static void main(String[] args) {
         System.out.println("Игра “Угадай число”");
-        int pcNum = 1;
-        int userNum = 50;
-        int effortNum = 1;
+        int secretNum = 75;
+        int playerNum = 50;
+        int attempts = 1;
         System.out.println("Привет, я загадал число от 1 до 100. Попробуй угадать его!");
-        while (true) {
-            if (userNum < pcNum) {
-                System.out.printf("Попытка №%d. Число %d больше того, что я загадал\n", effortNum, userNum);
-                userNum +=2;
-                effortNum++;
-            } else if (userNum > pcNum) {
-                System.out.printf("Попытка №%d. Число %d меньше того, что я загадал\n", effortNum, userNum);
-                userNum -=1;
-                effortNum++;
-            } else if (userNum == pcNum) {
-                System.out.printf("Ура, Вы победили! Загаданное число было: %d.\nВсего попыток было: %d", 
-                        userNum, effortNum);
-                break;
+        while (playerNum != secretNum) {
+            if (playerNum < secretNum) {
+                System.out.printf("Попытка №%d. Число %d больше того, что я загадал\n", attempts, playerNum);
+                playerNum +=2;
+            } else if (playerNum > secretNum) {
+                System.out.printf("Попытка №%d. Число %d меньше того, что я загадал\n", attempts, playerNum);
+                playerNum--;
+            attempts++;
             }
         }
+        System.out.printf("Ура, Вы победили! Загаданное число было: %d.\nВсего попыток было: %d", 
+                        playerNum, attempts);
     }
 }
