36d56a3e870c3406703742566359d995ca0b113a
Wenzel <kachman87@gmail.com>
2022-11-26 21:48:26 +0400

    Add Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,33 @@
+public class Person {
+
+    String name = "Vasya";
+    String sex = "Male";
+    double height = 1.75;
+    double weight = 80.5;
+    int age = 33;
+
+    void walk() {
+        System.out.println("Человек гуляет");
+    }
+
+    boolean isSit() {
+        if (true) {
+            System.out.println("Человек сидит");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void run() {
+        System.out.println("Человек бежит");
+    }
+
+    void sayHello() {
+        System.out.println("Человек говорит - Привет!");
+    }
+
+    String learn() {
+        return "Java";
+    }
+}
