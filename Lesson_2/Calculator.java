9033114d7b76d8adc8cc559789662f3c50b45816
Wenzel <kachman87@gmail.com>
2022-11-29 08:04:14 +0400

    5th refactoring Calculator

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -5,24 +5,12 @@ class Calculator {
     private char sign;
     private double result;
 
-    public int getNum1() {
-        return num1;
-    }
-
     public void setNum1(int num1) {
         this.num1 = num1;
     }
 
-    public int getNum2() {
-        return num2;
-    }
-
     public void setNum2(int num2) {
         this.num2 = num2;
-    }
-
-    public char getSign() {
-        return sign;
     }
 
     public void setSign(char sign) {
@@ -32,21 +20,20 @@ class Calculator {
     public double calculate() {
         switch (sign) {
             case '+': 
-                return result = num1 + num2;
+                return num1 + num2;
             case '-':
-                result = num1 - num2;
-                break;
+                return num1 - num2;
             case '*':
-                return result = num1 * num2;
+                return num1 * num2;
             case '/':
-                return result = (double) num1 / num2;
+                return (double) num1 / num2;
             case '^':
                 for (int i = 0; i < num2; i++) {
                     result *= num1;
                 }
                 return result;
             case '%':
-                return result = num1 % num2;
+                return num1 % num2;
         }
         return 0;
     }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -8,15 +8,15 @@ class CalculatorTest {
         String userAnswer = "yes";
         while (userAnswer.equals("yes")) {
             System.out.print("Введите первое число: ");
-            calc.setNum1(input.nextInt());
+            int num1 = input.nextInt();
+            calc.setNum1(num1);
             System.out.print("Введите знак математической операции: ");
-            calc.setSign(input.next().charAt(0));
+            char sign = input.next().charAt(0);
+            calc.setSign(sign);
             System.out.print("Введите второе число: ");
-            calc.setNum2(input.nextInt());
+            int num2 = input.nextInt();
+            calc.setNum2(num2);
             double result = calc.calculate();
-            int num1 = calc.getNum1();
-            int num2 = calc.getNum2();
-            char sign = calc.getSign();
 
             if (sign == '/' && num1 % num2 != 0) {
                 System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
