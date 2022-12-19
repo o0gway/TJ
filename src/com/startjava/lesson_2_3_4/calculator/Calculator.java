fea5c1a3d6519b63831f454c9e7753e6bc5e6462
Wenzel <kachman87@gmail.com>
2022-12-19 18:49:40 +0300

    Change switch with lambda expression

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -7,20 +7,14 @@ class Calculator {
     public static double calculate(String[] expression) {
         int num1 = Integer.parseInt(expression[0]);
         int num2 = Integer.parseInt(expression[2]);
-        switch (expression[1].charAt(0)) {
-            case '+': 
-                return num1 + num2;
-            case '-':
-                return num1 - num2;
-            case '*':
-                return num1 * num2;
-            case '/':
-                return (double) num1 / num2;
-            case '^':
-                return Math.pow(num1, num2);
-            case '%':
-                return num1 % num2;
-        }
-        return 0;
+        return switch (expression[1].charAt(0)) {
+            case '+' -> num1 + num2;
+            case '-' -> num1 - num2;
+            case '*' -> num1 * num2;
+            case '/' -> (double) num1 / num2;
+            case '^' -> Math.pow(num1, num2);
+            case '%' -> num1 % num2;
+            default -> 0;
+        };
     }
 }
